ea28a309f03594f9552f45b7595e4e4e25451292
obibaadoma <akrasikonadu@yahoo.com>
2024-03-28 19:41:25 +0000

    added color changes

--- sample-app/src/components/accordion/index.jsx
+++ sample-app/src/components/accordion/index.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import data from './data';
 import './styles.css';
 
-export default function Accordion() {
+export default function Accordian() {
   const [selected, setSelected] = useState(null);
   const [enableMultiSelection, setEnableMultiSelection] = useState(false);
   const [multiple, setMultiple] = useState([]);
@@ -11,29 +11,31 @@ export default function Accordion() {
     setSelected(getCurrentId === selected ? null : getCurrentId);
   }
 
-  function handleMultipleSelection(getCurrentId) {
-    const cpyMultiple = [...multiple];
-    const findIndexOfCurrentId = cpyMultiple.indexOf(getCurrentId);
+  function handleMultiSelection(getCurrentId) {
+    const cpyMutiple = [...multiple];
+    const findIndexOfCurrentId = cpyMutiple.indexOf(getCurrentId);
 
-    if (findIndexOfCurrentId === -1) {
-      cpyMultiple.push(getCurrentId);
-    } else {
-      cpyMultiple.splice(findIndexOfCurrentId, 1);
-    }
+    console.log(findIndexOfCurrentId);
+    if (findIndexOfCurrentId === -1) cpyMutiple.push(getCurrentId);
+    else cpyMutiple.splice(findIndexOfCurrentId, 1);
 
-    setMultiple(cpyMultiple);
+    setMultiple(cpyMutiple);
   }
+
+  console.log(selected, multiple);
   return (
     <div className="wrapper">
-      <button onClick={() => setEnableMultiSelection(!enableMultiSelection)}>Enable Multi Selection</button>
+      <button onClick={() => setEnableMultiSelection(!enableMultiSelection)}>
+        Enable Multi Selection
+      </button>
       <div className="accordion">
-        { data && data.length > 0 ? (
+        {data && data.length > 0 ? (
           data.map((dataItem) => (
-            <div className="item" key={dataItem.id}>
+            <div className="item">
               <div
                 onClick={
                   enableMultiSelection
-                    ? () => handleMultipleSelection(dataItem.id)
+                    ? () => handleMultiSelection(dataItem.id)
                     : () => handleSingleSelection(dataItem.id)
                 }
                 className="title"
@@ -41,16 +43,18 @@ export default function Accordion() {
                 <h3>{dataItem.question}</h3>
                 <span>+</span>
               </div>
+              {enableMultiSelection
+                ? multiple.indexOf(dataItem.id) !== -1 && (
+                <div className="acc-content ">{dataItem.answer}</div>
+                )
+                : selected === dataItem.id && (
+                <div className="acc-content ">{dataItem.answer}</div>
+                )}
 
-              {selected === dataItem.id || multiple.indexOf(dataItem.id) !== -1 ? (
-                <div className="content">
-                  {dataItem.answer}
-                </div>
-              ) : null}
             </div>
           ))
         ) : (
-          <div><h1>No Data Available!!</h1></div>
+          <div>No data found !</div>
         )}
       </div>
     </div>
---
+++ sample-app/src/components/random-color/index.jsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from 'react';
+import './styleColor.css';
+
+export default function RandomColor() {
+  const [typeOfColor, setTypeOfColor] = useState('#000000');
+  const [color, setColor] = useState(' ');
+
+  function randomColorUtility(length) {
+    return Math.floor(Math.random() * length);
+  }
+
+  function handleCreateRandomHexColor() {
+    // #678765
+    const hex = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+    let hexColor = '#';
+
+    for (let i = 0; i < 6; i + 1) {
+      hexColor += hex[randomColorUtility(hex.length)];
+    }
+    setColor(hexColor);
+  }
+
+  function handleCreateRandomRgbColor() {
+    const r = randomColorUtility(256);
+    const g = randomColorUtility(256);
+    const b = randomColorUtility(256);
+
+    setColor(`rgb(${r}, ${g}, ${b})`);
+  }
+
+  useEffect(() => {
+    if (typeOfColor === 'rgb') handleCreateRandomRgbColor();
+    else handleCreateRandomHexColor();
+  }, [typeOfColor]);
+
+  function handleCreateRandomColor() {
+    if (typeOfColor === 'hex') {
+      handleCreateRandomHexColor();
+    } else {
+      handleCreateRandomRgbColor();
+    }
+  }
+
+  return (
+    <div className="color-container">
+      <button type="button" onClick={() => setTypeOfColor('hex')}>Create Hex Color</button>
+      <button type="button" onClick={() => setTypeOfColor('rgb')}>Create RGB Color</button>
+      <button type="button" onClick={typeOfColor === 'hex' ? handleCreateRandomHexColor : handleCreateRandomColor}>Generate Random Color</button>
+
+      <div className="color-display">
+        <h3>{typeOfColor === 'rgb' ? 'RBB Color' : 'HEX Color'}</h3>
+        <h1>{color}</h1>
+      </div>
+    </div>
+  );
+}
